d2ebe150839bc98e53858be44626cd18654f3390
sergey <wendingnn@gmail.com>
2024-05-14 15:57:50 +0300

    task-4

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
-import readlineSync from 'readline-sync';
-const greeting = () =>{
-	const name = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${name}!`);
-}
+import readlineSync from "readline-sync";
+const greeting = () => {
+  const name = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${name}!`);
+};
 export default greeting;
-	
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 import _ from "lodash";
 import start from "./src/cli.js";
 console.log("Hello, Hexlet!");
+let b = 80;
+console.log(b);
 _.capitalize("hello");
 console.log(_.last(["one", "two"]));
-start()
+start();
