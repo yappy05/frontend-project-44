9a7093cc8aa33794dd85447365b2ac30db1d966a
sergey <wendingnn@gmail.com>
2024-06-12 17:29:54 +0300

    fix-8

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import * as defaultMethods from "../src/index.js";
+import * as defaultMethods from '../src/index.js';
 
 const isPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
@@ -19,15 +19,15 @@ for (let j = 0; j < 3; j += 1) {
   }
   const numPrime = isPrime(randomNum);
   defaultMethods.questionMy(randomNum);
-  const answer = defaultMethods.answerMy() === "yes";
+  const answer = defaultMethods.answerMy() === 'yes';
   // console.log(`answer = ${answer}, prime = ${numPrime}`);
   flag = defaultMethods.makeTry(
     answer === numPrime,
     defaultMethods.uncorrectAnswer(
-      answer ? "no" : "yes",
-      answer ? "yes" : "no",
-      name
-    )
+      answer ? 'no' : 'yes',
+      answer ? 'yes' : 'no',
+      name,
+    ),
   );
   if (!flag) break;
 }
